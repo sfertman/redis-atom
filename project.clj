2fd4af7d792fa1efa52c0d3c66ac8c715bd1e876
sfertman <sfertman@zoho.com>
2019-08-09 19:58:24 -0400

    project: add carmine deps

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
     :name "MIT"
     :url "https://opensource.org/licenses/mit-license.php"}
   :dependencies [
+    [com.taoensso/carmine "2.19.1"]
     [org.clojure/clojure "1.10.0"]]
   :repl-options {:init-ns redis-atom.core})
