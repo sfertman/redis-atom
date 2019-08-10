3a9310acc9e24c15f050ee50694a6e442b60ba3a
sfertman <sfertman@zoho.com>
2019-08-10 18:28:33 -0400

    cosmetics

--- src/redis_atom/core.clj
+++ src/redis_atom/core.clj
@@ -1,6 +1,6 @@
 (ns redis-atom.core
   (:require
-    [taoensso.carmine :as car :refer [wcar]]))
+    [taoensso.carmine :as r]))
 
 (def conn {})
 
@@ -9,26 +9,26 @@
   current value of the atom is identical to oldval. Returns true if
   set happened, else false"
   [k oldval newval]
-  (car/watch k)
-  (if (not= oldval (car/get k))
+  (r/watch k)
+  (if (not= oldval (r/get k))
     (do
-      (car/unwatch)
+      (r/unwatch)
       false)
     (do
-      (car/multi)
-      (car/set k newval)
-      (some? (car/exec))))) ;; exec will return nil if failed
+      (r/multi)
+      (r/set k newval)
+      (some? (r/exec))))) ;; exec will return nil if failed
 
 (defn atom
   [k val]
-  (wcar conn (car/set k val)) ;; need to check for success
+  (r/wcar conn (r/set k val)) ;; need to check for success
   k)
 
 (defn reset!
   "Sets the value of atom to newval without regard for the
   current value. Returns newval."
   [k newval]
-  (wcar conn (car/set k newval)) ;; need to check for sucess;
+  (r/wcar conn (r/set k newval)) ;; need to check for sucess;
   newval)
 
 (defn swap!
@@ -37,9 +37,9 @@
   multiple times, and thus should be free of side effects.  Returns
   the value that was swapped in."
   [k f & args]
-  (wcar conn
-    (loop [oldval (car/get k)]
+  (r/wcar conn
+    (loop [oldval (r/get k)]
       (let [newval (apply f oldval args)]
         (if (compare-and-set! k oldval newval)
           newval
-          (recur (car/get k)))))))
+          (recur (r/get k)))))))
