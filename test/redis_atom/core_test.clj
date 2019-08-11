1885153f95a2c67bc82de159138debeaa526d62d
sfertman <sfertman@zoho.com>
2019-08-10 22:02:22 -0400

    silly test

--- test/redis_atom/core_test.clj
+++ test/redis_atom/core_test.clj
@@ -1,7 +1,22 @@
 (ns redis-atom.core-test
-  (:require [clojure.test :refer :all]
-            [redis-atom.core :refer :all]))
+  (:require
+    [clojure.pprint :refer [pprint]]
+    [clojure.test :refer :all]
+    [redis-atom.core :as r]
+    [taoensso.carmine :as redis]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+
+(def conn {
+  :pool {}
+  :spec {:uri "redis://localhost:6379"}})
+
+(def a (r/atom conn :k nil ))
+
+(pprint (r/reset! a {} ))
+
+(pprint (r/deref a))
+
+(pprint (r/reset! a {:a 42 :b 24}))
+
+(pprint (r/swap! a inc))
+(pprint (r/swap! a + 5))
