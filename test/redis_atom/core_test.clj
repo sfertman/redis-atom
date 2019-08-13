52444011d718a505fb3b58a3a3deef04d1eb3d43
sfertman <sfertman@zoho.com>
2019-08-13 00:23:00 -0400

    some basic tests

--- test/redis_atom/core_test.clj
+++ test/redis_atom/core_test.clj
@@ -1,6 +1,5 @@
 (ns redis-atom.core-test
   (:require
-    [clojure.pprint :refer [pprint]]
     [clojure.test :refer :all]
     [redis-atom.core :as r]
     [taoensso.carmine :as redis]))
@@ -10,13 +9,34 @@
   :pool {}
   :spec {:uri "redis://localhost:6379"}})
 
-(def a (r/atom conn :k nil ))
+(defmacro wcar* [& body] `(redis/wcar conn ~@body))
 
-(pprint (r/reset! a {} ))
+(wcar* (redis/flushall))
 
-(pprint (r/deref a))
+(deftest test-atom
+  (let [a (r/atom conn :test-atom 42)]
+    (is (= {:conn conn :key :test-atom} a))
+    (is (= {:data 42} (wcar* (redis/get :test-atom))))))
 
-(pprint (r/reset! a {:a 42 :b 24}))
+(deftest test-deref
+  (let [a (r/atom conn :test-deref 42)]
+    (is (= 42 (r/deref a)))))
 
-(pprint (r/swap! a inc))
-(pprint (r/swap! a + 5))
+(deftest test-reset
+  (let [a (r/atom conn :test-reset 42)]
+    (is (= 42 (r/deref a)))
+    (is (= 44 (r/reset! a 44)))
+    (is (= 44 (r/deref a)))))
+
+(deftest test-compare-and-set
+  (let [a (r/atom conn :test-compare-and-set 42)]
+    (is (= 42 (r/deref a)))
+    (is (false? (r/compare-and-set! a 57 44)))
+    (is (= 42 (r/deref a)))
+    (is (true? (r/compare-and-set! a 42 44)))
+    (is (= 44 (r/deref a)))))
+
+(deftest test-swap
+  (let [a (r/atom conn :test-swap 42)]
+    (is (= 42 (r/deref a)))))
+    ;; ^ this is where tests get fun --- continue here
