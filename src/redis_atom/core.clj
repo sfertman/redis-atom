f55e118be425ebeca315cb81e60b50b94a8ab6b7
sfertman <sfertman@zoho.com>
2019-11-07 21:49:11 -0500

    swapVals

--- src/redis_atom/core.clj
+++ src/redis_atom/core.clj
@@ -2,14 +2,14 @@
   (:require
     [taoensso.carmine :as r])
   (:import
-    clojure.lang.IAtom
+    clojure.lang.IAtom2
     clojure.lang.IDeref))
 
 (deftype RedisAtom [conn k]
   IDeref
   (deref [_] (:data (r/wcar conn (r/get k))))
 
-  IAtom
+  IAtom2
   (reset [_ newval]
     (r/wcar conn (r/set k {:data newval}))
     newval)
@@ -45,6 +45,30 @@
       (let [newval (apply f oldval x y args)]
         (if (compare-and-set! this oldval newval)
           newval
+          (recur @this)))))
+  (swapVals [this f]
+    (loop [oldval @this]
+      (let [newval (f oldval)]
+        (if (compare-and-set! this oldval newval)
+          [oldval newval]
+          (recur @this)))))
+  (swapVals [this f x]
+    (loop [oldval @this]
+      (let [newval (f oldval x)]
+        (if (compare-and-set! this oldval newval)
+          [oldval newval]
+          (recur @this)))))
+  (swapVals [this f x y]
+    (loop [oldval @this]
+      (let [newval (f oldval x y)]
+        (if (compare-and-set! this oldval newval)
+          [oldval newval]
+          (recur @this)))))
+  (swapVals [this f x y args]
+    (loop [oldval @this]
+      (let [newval (apply f oldval x y args)]
+        (if (compare-and-set! this oldval newval)
+          [oldval newval]
           (recur @this))))))
 
 (defn redis-atom
