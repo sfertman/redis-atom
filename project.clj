ec0148f8562a32c61afaf92915a34251115481a7
Setzer22 <jsanchezfsms@gmail.com>
2022-06-29 09:41:50 +0200

    Bump carmine version to 3.1.0

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
     :name "MIT"
     :url "https://opensource.org/licenses/mit-license.php"}
   :dependencies [
-    [com.taoensso/carmine "2.19.1"]
+    [com.taoensso/carmine "3.1.0"]
     [org.clojure/clojure "1.10.0"]]
   :repl-options {:init-ns redis-atom.core}
   :aot [redis-atom.RedisAtom])
